3364a1b5f784dbfc4566c79daf22ee8daf5711a4
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-05 19:36:46 -0800

    Add devise models for coach and athlete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :athletes
+  devise_for :coaches
   root to: "home#index"
 end
---
+++ spec/factories/athletes.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :athlete do
+    
+  end
+end
