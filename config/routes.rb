01a0979eb3915994aaa20675f9fa3b2a771d17e0
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-06 10:54:06 -0800

    New routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :athletes
+  
   devise_for :coaches, controllers: { registrations: "registrations" }
   root to: "home#index"
 
   resources :coaches, :only => [:show] do
-    resources :athletes
+    devise_for :athletes
   end
 
   post 'coaches/:id/invite', to: 'athletes#invite', as: :invite
