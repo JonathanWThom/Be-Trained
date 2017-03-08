08b4dffd5e3d704e0d0439c22fe798b24d03398e
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-08 10:26:36 -0800

    Add flash notice

--- app/controllers/athletes/registrations_controller.rb
+++ app/controllers/athletes/registrations_controller.rb
@@ -1,27 +1,27 @@
 class Athletes::RegistrationsController < Devise::RegistrationsController
-    # before_filter :configure_sign_up_params, only: [:create]
-    def create
-      build_resource(sign_up_params)
+  def create
+    build_resource(sign_up_params)
 
-      resource.save
-      yield resource if block_given?
-      if resource.persisted?
-        if resource.active_for_authentication?
-          set_flash_message! :notice, :signed_up
-          sign_up(resource_name, resource)
-          respond_with resource, location: after_sign_up_path_for(resource)
-        else
-          set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-          expire_data_after_sign_in!
-          respond_with resource, location: after_inactive_sign_up_path_for(resource)
-        end
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
       else
-        clean_up_passwords resource
-        set_minimum_password_length
-      
-        redirect_back(fallback_location: root_path)
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
       end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      ## change this to include real error messages eventually
+      flash[:notice] = "There were some problems with your registration."
+      redirect_back(fallback_location: root_path)
     end
+  end
 
 protected
 
--- app/controllers/coaches/registrations_controller.rb
+++ app/controllers/coaches/registrations_controller.rb
@@ -1,4 +1,27 @@
 class Coaches::RegistrationsController < Devise::RegistrationsController
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      ## change this to include real error messages eventually
+      flash[:notice] = "There were some problems with your registration."
+      redirect_back(fallback_location: root_path)
+    end
+  end
   protected
 
   def after_sign_up_path_for(coach)
