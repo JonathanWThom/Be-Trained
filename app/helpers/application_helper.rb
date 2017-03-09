372df514cc7879f3a42ccbb5a1e8ad16cac4af64
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-09 09:25:16 -0800

    Fix helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def valid_user
-    if !current_coach || !current_athlete || (current_coach && (current_coach != @athlete.coach)) || (current_athlete && (current_athlete != @athlete))
+    if (!current_coach && !current_athlete) || (current_coach && (current_coach != @athlete.coach)) || (current_athlete && (current_athlete != @athlete))
       true
     end
   end
