78186303fe6446dacac676a86bc97eaf907bd2e8
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-10 14:52:22 -0800

    More specific invalid email messages

--- app/controllers/athletes/passwords_controller.rb
+++ app/controllers/athletes/passwords_controller.rb
@@ -1,19 +1,19 @@
 class Athletes::PasswordsController < Devise::PasswordsController
 
-def create
-  self.resource = resource_class.send_reset_password_instructions(resource_params)
-  yield resource if block_given?
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    yield resource if block_given?
 
-  if successfully_sent?(resource)
-    respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
-  else
-    if resource_params[:email] = ''
-      flash[:notice] = "Email can\'t be blank"
+    if successfully_sent?(resource)
+      respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
     else
-      flash[:notice] = "Invalid email"
+      if resource_params[:email] == ''
+        flash[:notice] = "Email can\'t be blank"
+      else
+        flash[:notice] = "Email not found"
+      end
+      respond_with(resource)
     end
-    respond_with(resource)
   end
-end
 
 end
