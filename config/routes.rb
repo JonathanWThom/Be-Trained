6adaa71ff1d0024851dbdf4447a52aabf70621bf
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-10 15:06:51 -0800

    Restrict athlete sign up path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_scope :athlete do
-    get '/athletes/sign_up/:coach_id', :to => 'athletes/registrations#new'
+    get '/athletes/sign_up/:coach_id', :to => 'athletes/registrations#new', as: :new_coach_athlete_registration
   end
 
-  devise_for :athletes, :controllers => { registrations: 'athletes/registrations', passwords: 'athletes/passwords'}
+  devise_for :athletes, :controllers => { registrations: 'athletes/registrations', passwords: 'athletes/passwords'}, :path_names => { sign_up: ''}
 
   devise_for :coaches, controllers: { registrations: "coaches/registrations" }
   root to: "home#index"
