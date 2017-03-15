a62dbeeff425a9f3f9397f3a38219eae4ba59aad
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-15 14:14:27 -0700

    Change default url sender for password reset in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
     :authentication => :plain
   }
   ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'betrained@betrained.us' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
